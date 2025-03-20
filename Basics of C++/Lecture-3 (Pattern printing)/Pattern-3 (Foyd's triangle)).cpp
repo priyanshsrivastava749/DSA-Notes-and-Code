2e4c443d34e2450a100d1f9b58b9929a1225378e
Akshat22020 <158441640+Akshat22020@users.noreply.github.com>
2025-03-20 13:43:48 +0530

    Update and rename Pattern-3 (Foyd's triangle) to Pattern-3 (Foyd's triangle)).cpp

--- Basics of C++/Lecture-3 (Pattern printing)/Pattern-3 (Foyd's triangle)).cpp
+++ Basics of C++/Lecture-3 (Pattern printing)/Pattern-3 (Foyd's triangle)).cpp
@@ -13,10 +13,10 @@ int main(){
     }
 }
 
-op-
-Enter the number of rows:5
-1 
-2 3 
-4 5 6 
-7 8 9 10 
-11 12 13 14 15 
+// op-
+// Enter the number of rows:5
+// 1 
+// 2 3 
+// 4 5 6 
+// 7 8 9 10 
+// 11 12 13 14 15 
