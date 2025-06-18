294063926f46a737cf9697ea02d5a7f6bf163e95
Akshat2 <raj.akshat2202@gmail.com>
2025-06-18 10:37:47 +0530

    Added strings module 6 and LC-242 ,Valid anagram

---
+++ module-6(Strings)/Leetcode-242/code.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<String>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s==t) return true;
+        else return false;
+    }
+};
