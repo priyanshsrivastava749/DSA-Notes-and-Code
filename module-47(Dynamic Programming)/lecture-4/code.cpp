891f50fe2136de75584053ac50df3cde6547ca52
Priyansh <priyanshsrivastava749@gmail.com>
2025-11-11 21:07:26 +0530

    added the notes and code of leetcode 63

---
+++ module-47(Dynamic Programming)/lecture-4/code.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int helper(int sr,int sc,int er,int ec,vector<vector<int>>& dp){
+        if(sr == er && sc == ec) return 1;
+        if(sr > er || sc > ec)return 0;
+        if(dp[sr][sc] != -1) return dp[sr][sc];
+        int rightway = helper(sr,sc+1,er,ec,dp);
+        int downway = helper(sr+1,sc,er,ec,dp);
+        return dp[sr][sc] = rightway + downway;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return helper(0,0,m-1,n-1,dp);
+    }
+};
